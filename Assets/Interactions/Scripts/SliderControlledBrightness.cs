1900697605f13be2d950f6d17edcba025b40a10c
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-17 20:44:44 -0500

    changed the intensity of the light when using the slider

---
+++ Assets/Interactions/Scripts/SliderControlledBrightness.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderControlledBrightness : MonoBehaviour
+{
+    public Light light;
+    public float minIntensity;
+    public float maxIntensity;
+    public Slider slider;
+
+    public void OnSliderValueChanged()
+    {
+
+        light.intensity = Mathf.Lerp(minIntensity, maxIntensity, slider.value);
+    }
+}
