8dae36273e0cac6923b78c80b93faca4f4f7c2bb
Mayra Zamora <46287961+mayraazamora@users.noreply.github.com>
2023-03-17 20:46:53 -0500

    Calculate how far along the slider is as a percentage (0  to 1)

---
+++ Assets/Interactions/Scripts/Slider.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Slider : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float value;
+
+    public UnityEvent onValueChanged;
+    private float prevValue;
+
+    void Update()
+    {
+        // Calculate how far along the slider is as a percentage (0  to 1)
+        value = (transform.localPosition.x - minX) / (maxX - minX);
+        value = Mathf.Clamp(value, 0f, 1f);
+
+        // If the value changed 
+        if(value != prevValue)
+        {
+            Debug.Log("reached");
+
+            onValueChanged.Invoke();
+            prevValue = value; 
+        }
+    }
+}
